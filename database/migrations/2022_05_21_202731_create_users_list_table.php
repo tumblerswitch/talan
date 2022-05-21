acf616c1f7c07bbe8b61cf3fc45e46609d56f634
t.sharipov <t.sharipov@myhart.ru>
2022-05-21 23:53:15 +0300

    create users_list table

---
+++ database/migrations/2022_05_21_202731_create_users_list_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_list', function (Blueprint $table) {
+            $table->id();
+            $table->string('fio')->unique();
+            $table->string('email');
+            $table->string('phone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_list');
+    }
+};
